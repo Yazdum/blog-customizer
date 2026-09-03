610647d34c49a7e46510ac352d6db2c1dcdcbccb
Yazdum <bolmelcer@yandex.ru>
2026-09-04 02:05:19 +0300

    fix: address review comments

--- src/components/article-params-form/ArticleParamsForm.tsx
+++ src/components/article-params-form/ArticleParamsForm.tsx
@@ -8,12 +8,12 @@ import {
 	fontColors,
 	fontFamilyOptions,
 	fontSizeOptions,
-	OptionType,
 } from 'src/constants/articleProps';
 import { ArrowButton } from 'src/ui/arrow-button';
 import { Button } from 'src/ui/button';
 import { RadioGroup } from 'src/ui/radio-group';
 import { Select } from 'src/ui/select';
+import { Text } from 'src/ui/text';
 
 import styles from './ArticleParamsForm.module.scss';
 
@@ -26,31 +26,41 @@ export const ArticleParamsForm = ({
 	articleState,
 	onApply,
 }: ArticleParamsFormProps) => {
-	const [isOpen, setIsOpen] = useState(false);
+	const [isArticleParamsSidebarOpen, setIsArticleParamsSidebarOpen] =
+		useState(false);
 	const [formState, setFormState] = useState<ArticleStateType>(articleState);
-	const rootRef = useRef<HTMLDivElement>(null);
+	const articleParamsSidebarRef = useRef<HTMLDivElement>(null);
 
 	useEffect(() => {
-		if (!isOpen) {
+		if (!isArticleParamsSidebarOpen) {
 			return;
 		}
 
 		const handleOutsideClick = (event: MouseEvent) => {
-			if (!rootRef.current?.contains(event.target as Node)) {
-				setIsOpen(false);
+			if (
+				event.target instanceof Node &&
+				!articleParamsSidebarRef.current?.contains(event.target)
+			) {
+				setIsArticleParamsSidebarOpen(false);
 			}
 		};
 
 		document.addEventListener('mousedown', handleOutsideClick);
 
 		return () => document.removeEventListener('mousedown', handleOutsideClick);
-	}, [isOpen]);
+	}, [isArticleParamsSidebarOpen]);
 
-	const handleOptionChange = (
-		field: keyof ArticleStateType,
-		option: OptionType
-	) => {
-		setFormState((currentState) => ({ ...currentState, [field]: option }));
+	const handleOptionChange =
+		<Field extends keyof ArticleStateType>(field: Field) =>
+		(option: ArticleStateType[Field]) => {
+			setFormState((currentState) => ({
+				...currentState,
+				[field]: option,
+			}));
+		};
+
+	const toggleArticleParamsSidebar = () => {
+		setIsArticleParamsSidebarOpen((isSidebarOpen) => !isSidebarOpen);
 	};
 
 	const handleSubmit = (event: FormEvent<HTMLFormElement>) => {
@@ -65,49 +75,52 @@ export const ArticleParamsForm = ({
 	};
 
 	return (
-		<div ref={rootRef}>
-			<ArrowButton isOpen={isOpen} onClick={() => setIsOpen(!isOpen)} />
+		<div ref={articleParamsSidebarRef}>
+			<ArrowButton
+				isOpen={isArticleParamsSidebarOpen}
+				onClick={toggleArticleParamsSidebar}
+			/>
 			<aside
 				className={clsx(styles.container, {
-					[styles.container_open]: isOpen,
+					[styles.container_open]: isArticleParamsSidebarOpen,
 				})}>
 				<form
 					className={styles.form}
 					onSubmit={handleSubmit}
 					onReset={handleReset}>
-					<h2 className={styles.title}>Задайте параметры</h2>
+					<Text as='h2' size={31} weight={800} uppercase>
+						Задайте параметры
+					</Text>
 					<Select
 						title='Шрифт'
 						selected={formState.fontFamilyOption}
 						options={fontFamilyOptions}
-						onChange={(option) =>
-							handleOptionChange('fontFamilyOption', option)
-						}
+						onChange={handleOptionChange('fontFamilyOption')}
 					/>
 					<RadioGroup
 						name='font-size'
 						title='Размер шрифта'
 						selected={formState.fontSizeOption}
 						options={fontSizeOptions}
-						onChange={(option) => handleOptionChange('fontSizeOption', option)}
+						onChange={handleOptionChange('fontSizeOption')}
 					/>
 					<Select
 						title='Цвет шрифта'
 						selected={formState.fontColor}
 						options={fontColors}
-						onChange={(option) => handleOptionChange('fontColor', option)}
+						onChange={handleOptionChange('fontColor')}
 					/>
 					<Select
 						title='Цвет фона'
 						selected={formState.backgroundColor}
 						options={backgroundColors}
-						onChange={(option) => handleOptionChange('backgroundColor', option)}
+						onChange={handleOptionChange('backgroundColor')}
 					/>
 					<Select
 						title='Ширина контента'
 						selected={formState.contentWidth}
 						options={contentWidthArr}
-						onChange={(option) => handleOptionChange('contentWidth', option)}
+						onChange={handleOptionChange('contentWidth')}
 					/>
 					<div className={styles.bottomContainer}>
 						<Button title='Сбросить' htmlType='reset' type='clear' />
